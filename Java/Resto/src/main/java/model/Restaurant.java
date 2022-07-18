1f16770d373f321c07bbfce9d865841644bbea3d
Olivier <o.blaivie@coperbee.fr>
2022-07-18 11:43:56 +0200

    Models for TP Restau

--- Java/Resto/src/main/java/model/Restaurant.java
+++ Java/Resto/src/main/java/model/Restaurant.java
@@ -1,5 +1,67 @@
 package model;
 
-public class Restaurant {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Restaurant {
+	
+	protected String code;
+	protected String nom;
+	protected String adresse;
+	protected int nbTables;
+	protected boolean statut;
+	protected List<Salarie> salaries = new ArrayList<Salarie>();
+	
+	
+	public Restaurant() { }
+	
+	public Restaurant (String code, String nom, String adresse, int nbTables, boolean statut) {
+		this.code = code;
+		this.nom = nom;
+		this.adresse = adresse;
+		this.nbTables = nbTables;
+		this.statut = statut;
+	}
+	
+	
+	
+	
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public int getNbTables() {
+		return nbTables;
+	}
+	public void setNbTables(int nbTables) {
+		this.nbTables = nbTables;
+	}
+	public boolean isStatut() {
+		return statut;
+	}
+	public void setStatut(boolean statut) {
+		this.statut = statut;
+	}
+	public List<Salarie> getSalaries() {
+		return salaries;
+	}
+	public void setSalaries(List<Salarie> salaries) {
+		this.salaries = salaries;
+	}
+	
 }
--- Java/Resto/src/main/java/model/Salarie.java
+++ Java/Resto/src/main/java/model/Salarie.java
@@ -1,5 +1,60 @@
 package model;
 
 public class Salarie {
+	
+	protected int id;
+	protected String nom;
+	protected String prenom;
+	protected double salaire = 1645.58;
+	protected String codeResto;
+	
+	
+	public Salarie() {};
+	
+	public Salarie(String nom, String prenom, double salaire) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.salaire = salaire;
+	};
+	
+	public Salarie(int id, String nom, String prenom, double salaire) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.salaire = salaire;
+	};
 
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public double getSalaire() {
+		return salaire;
+	}
+	public void setSalaire(double salaire) {
+		this.salaire = salaire;
+	}
+	public String getCodeResto() {
+		return codeResto;
+	}
+	public void setCodeResto(String codeResto) {
+		this.codeResto = codeResto;
+	}
+	
 }
